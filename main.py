d5b13609e771e4be8729be09330130a40141ce8c
prince <princeihe123@gmail.com>
2023-10-02 12:38:27 +0100

    shows largest and smallest numbers

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-list = []
+numbers = []
+
 n = int(input('How many numbers: '))
 total = 0
 
+largest = None
+smallest = None
+
 for i in range(n):
     value = int(input('Enter value: '))
+
+    if largest is None or value > largest:
+        largest = value
+    if smallest is None or value < smallest:
+        smallest = value
+    
     total += value
 
-avg = total/n
+    numbers.append(value)
+
+avg = total / n
 
 print('Average is ' + str(avg))
+print('Largest number is ' + str(largest))
+print('Smallest number is ' + str(smallest))
